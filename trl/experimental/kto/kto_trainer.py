7ce37728c73f647baa2113284a43ef22d51efd7c
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2026-04-14 11:10:44 +0200

    Align KTO with DPO: Add precompute_ref_batch_size (#5530)

--- trl/experimental/kto/kto_trainer.py
+++ trl/experimental/kto/kto_trainer.py
@@ -735,18 +735,24 @@ class KTOTrainer(_BaseTrainer):
             self.kto_loss_fn = LigerFusedLinearKTOLoss(beta=self.beta, use_ref_model=(self.ref_model is not None))
 
         if self.precompute_ref_log_probs:
-            self.train_dataset = self._precompute_reference_log_probs(
-                self.train_dataset, "train", self.args.per_device_train_batch_size
+            self.train_dataset = self._precompute_ref_logps(
+                self.train_dataset,
+                "train",
+                self.args.precompute_ref_batch_size or self.args.per_device_train_batch_size,
             )
             if self.eval_dataset is not None:
                 if isinstance(self.eval_dataset, dict):
                     self.eval_dataset = {
-                        name: self._precompute_reference_log_probs(dataset, name, self.args.per_device_eval_batch_size)
+                        name: self._precompute_ref_logps(
+                            dataset, name, self.args.precompute_ref_batch_size or self.args.per_device_eval_batch_size
+                        )
                         for name, dataset in self.eval_dataset.items()
                     }
                 else:
-                    self.eval_dataset = self._precompute_reference_log_probs(
-                        self.eval_dataset, "eval", self.args.per_device_eval_batch_size
+                    self.eval_dataset = self._precompute_ref_logps(
+                        self.eval_dataset,
+                        "eval",
+                        self.args.precompute_ref_batch_size or self.args.per_device_eval_batch_size,
                     )
 
     @contextmanager
@@ -763,7 +769,7 @@ class KTOTrainer(_BaseTrainer):
             if self.ref_adapter_name:
                 self.model.set_adapter(self.model_adapter_name or "default")
 
-    def _precompute_reference_log_probs(self, dataset: Dataset, name: str, batch_size: int) -> Dataset:
+    def _precompute_ref_logps(self, dataset: Dataset, name: str, batch_size: int) -> Dataset:
         dataloader_params = {
             "batch_size": batch_size,
             "collate_fn": self.data_collator,
